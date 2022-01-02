bcb8a8a1b2fb68af5a1222ff2cf12e5101d40f38
Fumiho <toshiyuki1998@gmail.com>
2022-01-02 18:51:51 +0900

    fit aws commit

--- plzgg/programs/connection_tools.py
+++ plzgg/programs/connection_tools.py
@@ -1,5 +1,5 @@
 from . import config
 import psycopg2
 def connect_db():
-    connection = psycopg2.connect("dbname="+config.DB_NAME+" user="+config.DB_USERNAME+" password="+config.DB_PASSWORD)
+    connection = psycopg2.connect("host=10.0.2.10 dbname="+config.DB_NAME+" user="+config.DB_USERNAME+" password="+config.DB_PASSWORD)
     return connection
